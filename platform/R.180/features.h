7dafa437aff04d901d639613cb8b5161a815f68d
kitor <github@kitor.pl>
2021-06-03 11:21:03 +0200

    EOSR, maybe DIGIC8: New shutter count property

--- platform/R.180/features.h
+++ platform/R.180/features.h
@@ -5,11 +5,22 @@
 #define FEATURE_COMPOSITOR_XCM
 #endif
 
+// Don't Click Me menu looks to be intended as a place
+// for devs to put custom code in debug.c run_test(),
+// and allowing triggering from a menu context.
+#define FEATURE_DONT_CLICK_ME
+
+#define FEATURE_SHOW_SHUTTER_COUNT
+
+// working but incomplete, some allocators don't report
+// anything yet as they're faked / not yet found
+#define FEATURE_SHOW_FREE_MEMORY
+
 #define CONFIG_ADDITIONAL_VERSION
 #define FEATURE_SCREENSHOT
-
 
 #undef CONFIG_CRASH_LOG
 #undef CONFIG_TSKMON
 #undef CONFIG_PROP_REQUEST_CHANGE
 #undef CONFIG_AUTOBACKUP_ROM
+

0317d7108240faf835406808f6d4c8fd2f320329
coon <coon@mailbox.org>
2022-01-17 14:12:40 +0100

    doom: load ML into clean HDMI buffer / ignore ml_reserved_mem

--- src/boot-d78.c
+++ src/boot-d78.c
@@ -91,11 +91,14 @@ copy_and_restart( int offset )
     // Note: unlike most (all?) DIGIC 4/5 cameras,
     // the malloc heap is specified as start + size (not start + end)
     // easiest way is to reduce its size and load ML right after it
-    ml_reserved_mem = 0x46000;
+    ml_reserved_mem = 569920;
+
+    /*
     qprint("[BOOT] reserving memory: "); qprintn(ml_reserved_mem); qprint("\n");
     qprint("before: user_mem_size = "); qprintn(INSTR(HIJACK_INSTR_HEAP_SIZE)); qprint("\n");
     INSTR( HIJACK_INSTR_HEAP_SIZE ) -= ml_reserved_mem;
     qprint(" after: user_mem_size = "); qprintn(INSTR(HIJACK_INSTR_HEAP_SIZE)); qprint("\n");
+    */
 
     // Fix cache maintenance calls before cstart
     FIXUP_BRANCH( HIJACK_FIXBR_DCACHE_CLN_1, my_dcache_clean );
